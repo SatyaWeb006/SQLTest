242a9cb8772fca7878ef5e666bfe7da2f7622d95
SatyaWeb006 <87054965+SatyaWeb006@users.noreply.github.com>
2021-07-07 11:02:02 +0530

    Update Test1.sql

--- Test1.sql
+++ Test1.sql
@@ -73,3 +73,34 @@ update  enrollments set year = 2015 where id >= 20 and id <= 100;
 select name from dogs
 union
 select name from cats;
+
+
+-- App usage data are kept in the following table:
+--
+-- TABLE sessions
+--   id INTEGER PRIMARY KEY,
+--   userId INTEGER NOT NULL,
+--   duration DECIMAL NOT NULL
+--
+-- Write a query that selects userId and average session duration for each user who has more than one session.
+
+-- Suggested testing environment:
+-- http://sqlite.online/
+
+-- Example case create statement:
+-- CREATE TABLE sessions (
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   userId INTEGER NOT NULL,
+--   duration DECIMAL NOT NULL
+-- );
+--
+-- INSERT INTO sessions(id, userId, duration) VALUES(1, 1, 10);
+-- INSERT INTO sessions(id, userId, duration) VALUES(2, 2, 18);
+-- INSERT INTO sessions(id, userId, duration) VALUES(3, 1, 14);
+--
+-- Expected output:
+-- UserId  AverageDuration
+-- -----------------------
+-- 1       12
+
+select userId, avg (duration) from sessions group by userId having count(userId) > 1;
