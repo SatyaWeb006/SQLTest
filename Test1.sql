ad1f42b4c6b5ff047189ad8cff92a91076147d23
SatyaWeb006 <87054965+SatyaWeb006@users.noreply.github.com>
2021-07-07 11:00:44 +0530

    Update Test1.sql

--- Test1.sql
+++ Test1.sql
@@ -30,3 +30,46 @@ update  enrollments set year = 2015 where id >= 20 and id <= 100;
 -- Write a query that updates the field 'year' of every faulty record to 2015.
 
 update  enrollments set year = 2015 where id >= 20 and id <= 100;
+
+
+-- Information about pets is kept in two separate tables:
+--
+-- TABLE dogs
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL
+--
+-- TABLE cats
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL
+--
+-- Write a query that select all distinct pet names.
+--
+-- Suggested testing environment:
+-- http://sqlite.online/
+
+-- Example case create statement:
+-- CREATE TABLE dogs (
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL
+-- );
+--
+-- CREATE TABLE cats (
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL
+-- );
+--
+-- INSERT INTO dogs(id, name) values(1, 'Lola');
+-- INSERT INTO dogs(id, name) values(2, 'Bella');
+-- INSERT INTO cats(id, name) values(1, 'Lola');
+-- INSERT INTO cats(id, name) values(2, 'Kitty');
+--
+-- Expected output (in any order):
+-- name
+-- -----
+-- Bella
+-- Kitty
+-- Lola
+
+select name from dogs
+union
+select name from cats;
